0f619e85127b9783ff45fbf93c2115f90d6ee659
Alice Bevan–McGregor <alice@gothcandy.com>
2014-07-02 17:13:09 -0400

    Added travis_retry to avoid intermittent network errors.

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,6 +5,7 @@ set -x
 git config --global user.email "alice+travis@gothcandy.com"
 git config --global user.name "Travis: Marrow"
 
-pip install --upgrade setuptools
-pip install tox
-pip install python-coveralls
+travis_retry pip install --upgrade setuptools pytest
+travis_retry pip install tox
+travis_retry pip install python-coveralls
+travis_retry pip install pytest-cov
