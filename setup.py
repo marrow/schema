32860df67c7f709c02cca06b023ad942f0a6ec17
Alice Bevan–McGregor <alice@gothcandy.com>
2014-07-02 15:24:35 -0400

    Updated release information and metadata.

--- setup.py
+++ setup.py
@@ -3,62 +3,86 @@
 
 import os
 import sys
+import codecs
+import warnings
 
-from setuptools import setup, find_packages
+try:
+	from setuptools.core import setup, find_packages
+except ImportError:
+	from setuptools import setup, find_packages
+
+from setuptools.command.test import test as TestCommand
 
 
 if sys.version_info < (2, 6):
-    raise SystemExit("Python 2.6 or later is required.")
+	raise SystemExit("Python 2.6 or later is required.")
+elif sys.version_info > (3, 0) and sys.version_info < (3, 2):
+	raise SystemExit("Python 3.2 or later is required.")
 
 exec(open(os.path.join("marrow", "schema", "release.py")).read())
 
 
+class PyTest(TestCommand):
+	def finalize_options(self):
+		TestCommand.finalize_options(self)
+		
+		self.test_args = []
+		self.test_suite = True
+	
+	def run_tests(self):
+		import pytest
+		sys.exit(pytest.main(self.test_args))
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+tests_require = ['pytest', 'pytest-cov']
 
 setup(
-        name = "marrow.schema",
-        version = version,
-
-        description = "A universal declarative syntax.",
-        long_description = """\
-For full documentation, see the README.textile file present in the package,
-or view it online on the GitHub project page:
-
-https://github.com/marrow/marrow.schema""",
-
-        author = "Alice Bevan-McGregor",
-        author_email = "alice+marrow@gothcandy.com",
-        url = "https://github.com/marrow/marrow.schema",
-        license = "MIT",
-
-        install_requires = [
-            'marrow.util < 2.0'
-        ],
-
-        test_suite = 'nose.collector',
-        tests_require = [
-            'nose',
-            'coverage'
-        ],
-
-        classifiers = [
-            "Development Status :: 5 - Production/Stable",
-            "Environment :: Console",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2.6",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.1",
-            "Programming Language :: Python :: 3.2",
-            "Topic :: Software Development :: Libraries :: Python Modules"
-        ],
-
-        packages = find_packages(exclude=['examples', 'tests']),
-        zip_safe = True,
-        include_package_data = True,
-        package_data = {'': ['README.textile', 'LICENSE']},
-
-        namespace_packages = ['marrow'],
-    )
+	name = "marrow.schema",
+	version = version,
+	
+	description = description,
+	long_description = codecs.open(os.path.join(here, 'README.rst'), 'r').read(),
+	url = url,
+	
+	author = author.name,
+	author_email = author.email,
+	
+	license = 'MIT',
+	keywords = '',
+	classifiers = [
+			"Development Status :: 5 - Production/Stable",
+			"Environment :: Console",
+			"Intended Audience :: Developers",
+			"License :: OSI Approved :: MIT License",
+			"Operating System :: OS Independent",
+			"Programming Language :: Python",
+			"Programming Language :: Python :: 2.7",
+			"Programming Language :: Python :: 3",
+			"Programming Language :: Python :: 3.2",
+			"Programming Language :: Python :: 3.3",
+			"Programming Language :: Python :: 3.4",
+			"Programming Language :: Python :: Implementation :: CPython",
+			"Programming Language :: Python :: Implementation :: PyPy",
+			"Topic :: Software Development :: Libraries :: Python Modules"
+		],
+	
+	packages = find_packages(exclude=['test', 'script']),
+	include_package_data = True,
+	namespace_packages = ['marrow'],
+	
+	install_requires=[
+		],
+	
+	extras_require = dict(
+			development = tests_require,
+		),
+	
+	tests_require = tests_require,
+	
+	zip_safe = False,
+	cmdclass = dict(
+			test = PyTest,
+		)
+)
