7de9015b11d50877b37aa52427d36fb36f48c6b8
Alice Bevan–McGregor <alice@gothcandy.com>
2014-07-02 15:23:25 -0400

    Updated testing environment.

---
+++ .travis/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+set -x
+
+git config --global user.email "alice+travis@gothcandy.com"
+git config --global user.name "Travis: Marrow"
+
+pip install --upgrade setuptools
+pip install tox
