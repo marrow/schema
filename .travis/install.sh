0573175b3a60ea2948b795476345f5b3d0beb707
Alice Bevan–McGregor <alice@gothcandy.com>
2014-07-02 17:17:21 -0400

    Moved travis_retry into the YAML config.  It is not real.

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,7 +5,7 @@ set -x
 git config --global user.email "alice+travis@gothcandy.com"
 git config --global user.name "Travis: Marrow"
 
-travis_retry pip install --upgrade setuptools pytest
-travis_retry pip install tox
-travis_retry pip install python-coveralls
-travis_retry pip install pytest-cov
+pip install --upgrade setuptools pytest
+pip install tox
+pip install python-coveralls
+pip install pytest-cov
