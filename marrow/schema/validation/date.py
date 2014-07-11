7a60ee4e4272235a540b6d28becff82a1c18967e
Alice Bevan–McGregor <alice@gothcandy.com>
2014-07-11 14:40:41 -0400

    Implemented date-related validators.

--- marrow/schema/validation/date.py
+++ marrow/schema/validation/date.py
@@ -1,8 +1,21 @@
-"""
+# encoding: utf-8
 
-VDate
-VTime
+from datetime import datetime, date, time, timedelta
 
-min/max
+from .base import Instance, Range
 
-"""
+
+class Date(Instance, Range):
+	instance = (datetime, date)
+
+
+class Time(Instance, Range):
+	instance = (datetime, time)
+
+
+class DateTime(Instance, Range):
+	instance = (datetime, )
+
+
+class Delta(Instance, Range):
+	instance = (timedelta, )
