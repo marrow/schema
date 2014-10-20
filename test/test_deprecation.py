97191e206e67e2ce2271a989921d2fdcdf73800f
Alice Bevan–McGregor <alice@gothcandy.com>
2014-10-20 16:09:18 -0400

    Parametrized the deprecation test.

--- test/test_deprecation.py
+++ test/test_deprecation.py
@@ -7,23 +7,21 @@ from marrow.schema.compat import unicode
 from marrow.schema.declarative import BaseAttribute, BaseDataAttribute
 
 
-class TestDeprecatedClasses:
-	def test_base_attribute(self):
-		with warnings.catch_warnings(record=True) as w:
-			warnings.simplefilter('always')
-			
-			BaseAttribute()
-			
-			assert len(w) == 1, "Only one warning should be raised."
-			assert issubclass(w[-1].category, DeprecationWarning), "Warning must be a DeprecationWarning."
-			assert "Container" in unicode(w[-1].message), "Warning should mention correct class to use."
+DEPRECATED = (
+		(BaseAttribute, 'Container'),
+		(BaseDataAttribute, 'DataAttribute'),
+	)
+
+
+@pytest.mark.parametrize('value', DEPRECATED)
+def test_deprecated(value):
+	cls, dst = value
 	
-	def test_base_data_attribute(self):
-		with warnings.catch_warnings(record=True) as w:
-			warnings.simplefilter('always')
-			
-			BaseDataAttribute()
-			
-			assert len(w) == 1, "Only one warning should be raised."
-			assert issubclass(w[-1].category, DeprecationWarning), "Warning must be a DeprecationWarning."
-			assert "DataAttribute" in unicode(w[-1].message), "Warning should mention correct class to use."
+	with warnings.catch_warnings(record=True) as w:
+		warnings.simplefilter('always')
+		
+		cls()
+		
+		assert len(w) == 1, "Only one warning should be raised."
+		assert issubclass(w[-1].category, DeprecationWarning), "Warning must be a DeprecationWarning."
+		assert dst in unicode(w[-1].message), "Warning should mention correct class to use."
