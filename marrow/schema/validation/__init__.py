9756375041f117cf25664856d3fb8932af795742
Alice Bevan–McGregor <alice@gothcandy.com>
2014-10-21 02:24:30 -0400

    Added imports for base objects.

--- marrow/schema/validation/__init__.py
+++ marrow/schema/validation/__init__.py
@@ -1,3 +1,9 @@
 # encoding: utf-8
 
 from .exc import Concern
+
+from .base import Validator, Callback, In, Contains, Length, Range, Pattern, Instance, Subclass, Equal
+from .base import Always, always, Never, never, Unique, unique
+from .base import AlwaysTruthy, truthy, Truthy, AlwaysFalsy, falsy, Falsy
+from .base import AlwaysRequired, required, Required, AlwaysMissing, missing, Missing
+from .base import ValidatedAttribute
