27f586ddd07204453d699c74c01e34ece22e7b4d
Alice Bevan–McGregor <alice@gothcandy.com>
2014-11-25 14:37:10 -0500

    Added StringIO adapter.

--- marrow/schema/compat.py
+++ marrow/schema/compat.py
@@ -14,6 +14,7 @@ import sys
 
 py2 = sys.version_info < (3, )
 py3 = sys.version_info > (3, )
+pypy = hasattr(sys, 'pypy_version_info')
 
 
 # ## Builtins Compatibility
@@ -22,11 +23,17 @@ if py3:  # pragma: no cover
 	native = str
 	unicode = str
 	str = bytes
+	iterkeys = dict.keys
+	itervalues = dict.values
+	iteritems = dict.items
 else:  # pragma: no cover
 	native = str
 	unicode = unicode
 	str = str
 	range = xrange
+	iterkeys = dict.iterkeys
+	itervalues = dict.itervalues
+	iteritems = dict.iteritems
 
 
 # ## Ordered Dictionaries
@@ -35,3 +42,14 @@ try:  # pragma: no cover
 	from collections import OrderedDict as odict
 except ImportError:  # pragma: no cover
 	from ordereddict import OrderedDict as odict
+
+
+# ## File-Like String Handling
+
+try:  # pragma: no cover
+	try:  # pragma: no cover
+		from cStringIO import StringIO
+	except ImportError:  # pragma: no cover
+		from StringIO import StringIO
+except ImportError:  # pragma: no cover
+	from io import StringIO
